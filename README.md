af5e135981b6858d031c53be1e4844e9e8c5b957
Francesca <francescamoore@gmail.com>
2025-05-17 22:11:02 -0700

    change how canvases drop in - add pizza layers

---
+++ README.md
@@ -0,0 +1,7 @@
+# CS 601 Week 2 Assignment - Drag & Drop Project
+
+Please take a look around. You can open index.html in your browser to view the game or launch it live in your IDE.
+
+## About My Project
+
+I decided to make a pizza topping game for my drag & drop project. I created two drop zones: a pizza and a trash can. To fill these drop zones, I created three groups in my json: actual pizza toppings, things that wouldn't go on pizza, and pineapple (since people either love or hate pineapple, it can go in either bucket). There are helpful prompts that show on screen when you sort correctly, incorrectly, or decide where the pineapple should go. Once all the toppings have been sorted, the toppings container hides and a message appears to indicate the game is over that that the user can refresh to play again. To keep the UI clean, I disable the button after the topping canvases load - otherwise you could add infinite canvases to the DOM and make it look cluttered. 
--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
+//API URL as constant
 const toppingsApi     = 'https://cramerfam.github.io/CS601_HW2_Cramer/models/toppings.json';
-const loadButton      = document.getElementById('loadCanvasButton');
+//DOM elements as constants
+const loadButton      = document.getElementById('loadToppingsButton');
 const canvasContainer = document.getElementById('canvasContainer');
+const dropZones       = ['pizzaDropZone', 'trashDropZone'];
+const infoZone        = document.getElementById('info');
+//Set event names as constants
+const CLICK      = 'click';
+const DRAG_START = 'dragstart';
+const DRAG_OVER  = 'dragover';
+const DROP       = 'drop';
+//Info messaging as constants
+const END_MESSAGE           = 'WHAT A PIZZA! PLEASE REFRESH TO PLAY AGAIN.';
+const FAIL_MESSAGE          = 'NOT QUITE. PLEASE TRY AGAIN!';
+const PINEAPPLE_MESSAGE     = 'PINEAPPLE - YOU EITHER LOVE IT OR HATE IT!';
+const SUCCESS_MESSAGE_PIZZA = 'YUM! THAT GOES GREAT ON PIZZA!';
+const SUCCESS_MESSAGE_TRASH = 'IN THE TRASH IT GOES, GOOD JOB!';
 
+//Fetch Toppings JSON from the API
 async function fetchToppingsData() {
     const response     = await fetch(toppingsApi);
     const toppingsJson = await response.json();
@@ -9,13 +25,23 @@ async function fetchToppingsData() {
     return toppingsJson;
 }
 
-loadButton.addEventListener('click', async () => {
+//Iterate over toppings JSON to create canvases of each topping
+loadButton.addEventListener(CLICK, async () => {
+
+    //Disable load button to prevent unlimited toppings
+    loadButton.disabled = true;
+    loadButton.classList.add('loaded');
+    loadButton.innerText = 'Toppings Loaded';
+
+
+    //Get toppings
     toppingsData = await fetchToppingsData();
 
+    //Iterate over categories in topics object
     for (let item in toppingsData) {
-
         let category = toppingsData[item];
 
+        //Create a canvas for each topping in each category
         category.forEach(topping => {
             const name   = topping.name;
             const type   = topping.goodFor;
@@ -27,8 +53,66 @@ loadButton.addEventListener('click', async () => {
             canvas.dataset.type = type;
             canvas.id = name;
 
+            //Set canvas data when drag begins
+            canvas.addEventListener(DRAG_START, (e) => {
+                e.dataTransfer.setData('text', canvas.id);
+                infoZone.innerText = '';
+            })
+
+            //Add the canvas to the container
             canvasContainer.appendChild(canvas);
         })
     }
 })
 
+//Iterate through drop zones
+dropZones.forEach(id => {
+
+    //Select the drop zone
+    const dropZone = document.getElementById(id);
+
+    //Prevent default behavior on drag over
+    dropZone.addEventListener(DRAG_OVER, (e) => {
+        e.preventDefault();
+    })
+
+    //Determine how to respond based on canvas data and drop zone
+    dropZone.addEventListener(DROP, (e) => {
+        e.preventDefault();
+
+        const canvasId   = e.dataTransfer.getData('text');
+        const canvas     = document.getElementById(canvasId);
+        const type       = canvas.dataset.type;
+        const dropPrefix = id.substring(0, 5);
+        const container  = type === 'either' ? document.getElementById(`${dropPrefix}Toppings`) : document.getElementById(`${type}Toppings`);
+
+        //If dropping real toppings on pizza, success!
+        if (id === 'pizzaDropZone' && type === 'pizza') {
+            container.appendChild(canvas); 
+            let overlay = document.getElementById(`pizza-${canvasId}`);
+            overlay.style.display = 'block';
+            infoZone.innerText = SUCCESS_MESSAGE_PIZZA;
+        } else if (id === 'trashDropZone' && type === 'trash'){
+            container.appendChild(canvas); 
+            infoZone.innerText = SUCCESS_MESSAGE_TRASH;
+        //User can put pineapple in either place
+        } else if (type === 'either') { 
+            container.appendChild(canvas); 
+            if (id === 'pizzaDropZone') {
+                let overlay = document.getElementById(`pizza-${canvasId}`);
+                overlay.style.display = 'block';
+            }
+            infoZone.innerText = PINEAPPLE_MESSAGE;
+        //Otherwise, show an error
+        } else {
+            infoZone.innerText = FAIL_MESSAGE;
+        }
+
+        //If the canvas container is empty, hide it and show a message
+        if (canvasContainer.children?.length === 0) {
+            canvasContainer.style.display = 'none';
+            loadButton.style.display = 'none';
+            infoZone.innerText = END_MESSAGE;
+        }
+    })
+});
